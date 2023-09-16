17b282a638e1eab2d71c982367c61ba660aa9042
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-16 16:12:23 -0700

    Access Triangle instance fields

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -9,6 +9,11 @@ namespace ShapeTracker
   {
     Triangle testTriangle = new Triangle();
     Console.WriteLine(testTriangle.GetType());
+    testTriangle.Side1 = 65;
+    testTriangle.Side3 = 88;
+    Console.WriteLine($"Side one of the triangle: {testTriangle.Side1}");
+    Console.WriteLine($"Side two of the triangle: {testTriangle.Side2}");
+    Console.WriteLine($"Side three of the triangle: {testTriangle.Side3}");
     }
   }
 }
