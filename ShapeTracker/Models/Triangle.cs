977e24f32cc5ed986a4d96802f92e43c4eb122be
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-16 16:06:14 -0700

    Add namespaces

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -1,4 +1,7 @@
-public class Triangle
+namespace ShapeTracker.Models
 {
+  public class Triangle
+  {
   // Code for Triangle business logic will go here.
+  }
 }
--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -1,9 +1,14 @@
 using System;
-class Program
+using ShapeTracker.Models;
+
+namespace ShapeTracker
 {
+  class Program
+  {
   static void Main()
   {
     Triangle testTriangle = new Triangle();
     Console.WriteLine(testTriangle.GetType());
+    }
   }
 }
