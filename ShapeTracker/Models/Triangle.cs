7197e2f9a7085de0e466c35a22372cefa8154b84
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-16 20:05:04 -0700

    Declare Triangle fields private

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -2,28 +2,28 @@ namespace ShapeTracker.Models
 {
   public class Triangle
   {
-    public int Side1;
-    public int Side2;
-    public int Side3;
+    private int _side1;
+    private int _side2;
+    private int _side3;
 
     public Triangle(int length1, int length2, int length3)
     {
-      Side1 = length1;
-      Side2 = length2;
-      Side3 = length3;
+      _side1 = length1;
+      _side2 = length2;
+      _side3 = length3;
     }
     
     public string CheckType()
     {
-      if ((Side1 > (Side2 + Side3)) || (Side2 > (Side1 + Side3)) || (Side3 > (Side1 + Side2)))
+      if ((_side1 > (_side2 + _side3)) || (_side2 > (_side1 + _side3)) || (_side3 > (_side1 + _side2)))
       {
         return "Not a triangle";
       }
-      else if ((Side1 != Side2) && ((Side1 != Side3)) && ((Side2 != Side3)))
+      else if ((_side1 != _side2) && ((_side1 != _side3)) && ((_side2 != _side3)))
       {
         return "Scalene triangle";
       }
-      else if ((Side1 == Side2) && (Side1 == Side3))
+      else if ((_side1 == _side2) && (_side1 == _side3))
       {
         return "Equilateral triangle";
       }
