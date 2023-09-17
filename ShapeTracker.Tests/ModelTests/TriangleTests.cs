3553b28338cfd1c9bc064ff47d0958880ad06cbd
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-17 14:15:47 -0700

    Create new test

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -12,5 +12,14 @@ namespace ShapeTracker.Tests
       Triangle newTriangle = new Triangle();
       Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
     }
+
+    [TestMethod]
+    public void GetSide1_ReturnsSide1_Int()
+    {
+      int length1 = 3;
+      Triangle newTriangle = new Triangle(length1);
+      int result = newTriangle.Side1;
+      Assert.AreEqual(length1, result);
+    }
   }
 }
